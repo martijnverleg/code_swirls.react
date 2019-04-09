2f41577ebb5ff25048387e96e1877dd765e22a40
Martijn Verleg <martijn@mrwatson.com>
2019-04-09 22:07:20 +0200

    deploy build to gh-pages

---
+++ precache-manifest.8da2b3d4364ea7f9d0d56135cca143aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d73ee17c30a1903282bc",
+    "url": "/static/js/main.47a7c3f2.chunk.js"
+  },
+  {
+    "revision": "e541129062b594cfea55",
+    "url": "/static/js/2.47b031fb.chunk.js"
+  },
+  {
+    "revision": "d73ee17c30a1903282bc",
+    "url": "/static/css/main.1a5ffbfe.chunk.css"
+  },
+  {
+    "revision": "ccf7a5a08424954c24a9d10d167e04d9",
+    "url": "/index.html"
+  }
+];
